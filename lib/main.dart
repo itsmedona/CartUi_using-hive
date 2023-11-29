d393696623a4985b8844a3b4e3f8bac94d2a5d6e
itsmedona <donashaji254@gmail.com>
2023-11-29 12:24:07 +0530

    edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:task_nov_24/model/item_model/item_model.dart';
 import 'package:task_nov_24/view/HomeCart/HomeCart.dart';
 //import 'package:task_nov_24/view/HomeCart/Shopping_Cart/Shopping_Cart.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(ItemModelAdapter());
+  var box = await Hive.openBox<ItemModel>('cart');
   runApp(MyCart());
 }
 
